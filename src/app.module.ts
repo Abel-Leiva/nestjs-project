c90488aa8c5340b034acc7f9231634e5586d3d92
Abel Leiva <a.ignacio.leivaleiva@gmail.com>
2024-01-26 07:56:21 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksModule } from './tasks/tasks.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'root',
+      password: 'root',
+      database: 'my_db',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      retryDelay: 3000,
+      retryAttempts: 10,
+    }),
+
+    TasksModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/tasks/controllers/tasks.controller.ts
@@ -0,0 +1,35 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { TasksService } from '../services/tasks.service';
+@Controller('api/tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+  @Get('all')
+  getAll() {
+    return this.taskService.findAll();
+  }
+  @Get(':id')
+  getOne(@Param('id') id: number) {
+    return this.taskService.findOne(id);
+  }
+  @Post()
+  create(@Body() body: any) {
+    return this.taskService.create(body);
+  }
+  @Put(':id')
+  update(@Param('id') id: number, @Body() body: any) {
+    return this.taskService.update(id, body);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.taskService.delete(id);
+  }
+}
